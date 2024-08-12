a2be50fbe1aa0188b2cbed7c829cf253187ffb1c
Shanmuga Suntharam <160557623+ShanmugaSuntharam@users.noreply.github.com>
2024-08-12 19:27:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 
 import streamlit as st
-from pages import home, grammar_fun, reading_translation, testAudioRecord
+from pages import home,  image_comprehension, grammar_fun, reading_translation, testAudioRecord
 
 PAGES = {
     "Home": home,
+    "Image Comprehenshion":image_comprehension,
     "Grammar and Fun": grammar_fun,
     "Reading and Translation": reading_translation,
     "Recorder Demo":testAudioRecord
