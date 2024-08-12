8bc79bc70b117a404d0f2a79ea595b58f0a5bbc7
Shanmuga Suntharam <160557623+ShanmugaSuntharam@users.noreply.github.com>
2024-08-12 14:22:55 +0530

    Update app.py
    added recorder demo page recorder_demo

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 
 import streamlit as st
-from pages import home, image_comprehension, grammar_fun, reading_translation
+from pages import home, image_comprehension, grammar_fun, reading_translation, recorder_demo
 
 PAGES = {
     "Home": home,
     "Image Comprehension": image_comprehension,
     "Grammar and Fun": grammar_fun,
-    "Reading and Translation": reading_translation
+    "Reading and Translation": reading_translation,
+    "Recorder Demo":recorder_demo
 }
 
 def main():
